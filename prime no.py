08b46b5f7fbbbbf578539a79b97f0e60748193bd
ShravyaShetty24 <shravyashetty022007@gmail.com>
2026-06-22 21:10:04 +0530

    Add files via upload
    Day 22/365

---
+++ prime no.py
@@ -0,0 +1,13 @@
+n = int(input("Enter number: "))
+
+flag = True
+
+for i in range(2, int(n**0.5)+1):
+    if n % i == 0:
+        flag = False
+        break
+
+if flag and n > 1:
+    print("Prime")
+else:
+    print("Not Prime")
