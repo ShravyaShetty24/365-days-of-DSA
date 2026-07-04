9b52ed20cca73bb0df0f60d8d6ecf1fc85d2535a
ShravyaShetty24 <shravyashetty022007@gmail.com>
2026-07-04 21:04:04 +0530

    Add files via upload
    Day 34/365

---
+++ Find the Leader in an Array.py
@@ -0,0 +1,9 @@
+arr = [16, 17, 4, 3, 5, 2]
+
+max_right = arr[-1]
+print(max_right, end=" ")
+
+for i in range(len(arr)-2, -1, -1):
+    if arr[i] > max_right:
+        max_right = arr[i]
+        print(max_right, end=" ")
