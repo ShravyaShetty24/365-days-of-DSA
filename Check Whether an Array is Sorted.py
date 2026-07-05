c9fe96b8880903c113897c1f17255d3fc4c86fad
ShravyaShetty24 <shravyashetty022007@gmail.com>
2026-07-05 21:18:38 +0530

    Add files via upload
    Day 35/365

---
+++ Check Whether an Array is Sorted.py
@@ -0,0 +1,13 @@
+arr = [1, 2, 3, 4, 5]
+
+sorted_array = True
+
+for i in range(len(arr) - 1):
+    if arr[i] > arr[i + 1]:
+        sorted_array = False
+        break
+
+if sorted_array:
+    print("Array is Sorted")
+else:
+    print("Array is Not Sorted")
